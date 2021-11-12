68ff4b0eb857175e1a3e0ba072f5ec63db79cfc6
syamsl19982 <syam.s.l1998@gmail.com>
2021-11-12 18:16:45 +0530

    first commit

---
+++ server/controllers/cloudinary.js
@@ -0,0 +1,32 @@
+const cloudinary = require('cloudinary')
+
+
+CLOUDINARY_CLOUD_NAME="dieknmxd6"
+CLOUDINARY_API_KEY=251919774281278;
+CLOUDINARY_API_SECRET="SfgQNqPqxs8lweoWZyR96O7kPJg"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+exports.upload = async (req, res) =>{
+    let result = await cloudinary.uploader.upload(req.body.image, {
+        public_id: `${Date.now()}`,
+        resource_type: 'auto'
+    })
+    res.json({
+        public_id: result.public_id,
+        url: result.secure_url,
+
+    })
+}
+
+exports.remove = (req, res) =>{
+    let image_id = req.body.public_id
+    cloudinary.uploader.destroy(image_id, (err, result) => {
+        if(err) return res.json({ success: false, err});
+        res.send("ok");
+    })
+}
